042c5f4b1864813d429892d9bc6d5849e2d71c22
ignaciopineyro <ignaciopineyro@DESKTOP-T00HTRB.localdomain>
2022-02-15 13:07:55 -0300

    Up to  lesson 4  4 completed

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,5 @@
-// let's go!
+import React from 'react'; // Usamos todo el paquete de React
+import { render } from 'react-dom'; // Usamos solo el metodo de render
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker />, document.querySelector('#main')); // Usar <Algo /> es como abrir y cerrar <Algo></Algo>
