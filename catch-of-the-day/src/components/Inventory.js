ca8a50417ed33244ebbddf6a51b3eb6fcc9816b6
ignaciopineyro <ignaciopineyro@DESKTOP-T00HTRB.localdomain>
2022-02-21 11:41:46 -0300

    Up to lesson 13 min 14:40 completed

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,9 +1,13 @@
 import React  from  "react";
+import AddFishFOrm from "./AddFishForm";
 
 class Inventory extends React.Component {
   render() {
     return (
-      <div className="inventory">Inventory!!</div>
+      <div className="inventory">
+        <h2>Inventory</h2>
+        <AddFishFOrm />
+      </div>
     );
   }
 }
